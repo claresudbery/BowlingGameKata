8bd5b195a944328f6c9dcb9bf63b013580ccbfff
Clare Sudbery <clare.sudbery@nice.org.uk>
2016-04-29 16:56:10 +0100

    First commit. Demonstrates the basic concept of TDD-like-you-mean-it, as opposed to test-first.

---
+++ BowlingGameKata/BowlingGameTests.cs
@@ -0,0 +1,50 @@
+﻿
+using NUnit.Framework;
+
+namespace BowlingGameKata
+{
+    [TestFixture]
+    public class BowlingGameTests
+    {
+        private int _score;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _score = 0;
+        }
+
+        [Test]
+        public void initial_score_is_zero()
+        {
+            AssertScoreEquals(0);
+        }
+
+        [Test]
+        public void total_score_is_score_from_first_try()
+        {
+            AddTry(3);
+
+            AssertScoreEquals(3);
+        }
+
+        [Test]
+        public void total_score_is_aggregate_score_of_all_tries()
+        {
+            AddTry(4);
+            AddTry(5);
+
+            AssertScoreEquals(9);
+        }
+
+        private void AddTry(int tryScore)
+        {
+            _score += tryScore;
+        }
+
+        private void AssertScoreEquals(int expected)
+        {
+            Assert.AreEqual(expected, _score);
+        }
+    }
+}
